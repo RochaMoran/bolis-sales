e830aadb3f6fb1362fc6fc4bed0be9ad263c52cc
RochaMoran <rochamoran1@gmail.com>
2023-06-12 19:39:14 -0600

    ADD: dashboard home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { DashboardItemModule } from 'src/app/components/dashboard-item/dashboard-item.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    DashboardItemModule
   ],
   declarations: [HomePage]
 })
