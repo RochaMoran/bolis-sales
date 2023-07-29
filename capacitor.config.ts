361f77665a2c94bdc32fa64d01f4db0e472e0c5a
RochaMoran <rochamoran1@gmail.com>
2023-07-29 15:41:05 -0600

    REMOVE: sqlite config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,25 +11,6 @@ const config: CapacitorConfig = {
     Network: {
       appId: "io.ionic.starter"
     },
-    CapacitorSQLite: {
-      iosDatabaseLocation: 'Library/CapacitorDatabase',
-      iosIsEncryption: true,
-      iosKeychainPrefix: 'angular-sqlite-app-starter',
-      iosBiometric: {
-        biometricAuth: false,
-        biometricTitle : "Biometric login for capacitor sqlite"
-      },
-      androidIsEncryption: true,
-      androidBiometric: {
-        biometricAuth : false,
-        biometricTitle : "Biometric login for capacitor sqlite",
-        biometricSubTitle : "Log in using your biometric"
-      },
-      electronIsEncryption: false,
-      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
-      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
-      electronLinuxLocation: "Databases"
-    }
   }
 };
 
